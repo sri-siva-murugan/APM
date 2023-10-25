143732885494b05c9a67b3e93f3f57b05602f176
Sri Siva Murugan V <srisivamurugan10@gmail.com>
2023-10-25 14:46:29 +0530

    implemented the show/hide image functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-
+import { ProductListComponent } from './products/product-list.component';
 @Component({
   selector: 'pm-root',
-  template: '<div><h1>{{pageTitle}}</h1><div>My First Component</div></div>',
+  template: `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <pm-products></pm-products>
+  </div>`,
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
